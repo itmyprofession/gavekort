3941cac06d2efd202e4e9cb44a5201d4d59ee148
Gopinath <gopinath.c@ewallsolutions.com>
2013-08-20 10:55:07 +0530

    Fixes #9

--- app/code/local/Ewall/Override/Model/Giftcert/Quote.php
+++ app/code/local/Ewall/Override/Model/Giftcert/Quote.php
@@ -2,6 +2,10 @@
 
 class Ewall_Override_Model_Giftcert_Quote extends Unirgy_Giftcert_Model_Quote
 {
+	/** Check quote item is virtual or not and return false if quote item is ugiftcert product
+	 * 
+	 * @return boolean true|false
+	 */
     public function isVirtual()
     {
         $isVirtual = true;
@@ -14,13 +18,7 @@ class Ewall_Override_Model_Giftcert_Quote extends Unirgy_Giftcert_Model_Quote
             $countItems ++;
 
             // If ugiftcert, check by quote item, not by product
-            if ($item->getProductType()=='ugiftcert') {
-                if (!$item->getProduct()->getTypeInstance()->isQuoteItemVirtual($item)) {
-                    $isVirtual = false;
-                } else {
-					$isVirtual = false;
-				}
-            } elseif (!$item->getProduct()->getIsVirtual()) {
+            if ($item->getProductType()=='ugiftcert' || !$item->getProduct()->getTypeInstance()->isQuoteItemVirtual($item) || !$item->getProduct()->getIsVirtual()) {
                 $isVirtual = false;
             }
         }
