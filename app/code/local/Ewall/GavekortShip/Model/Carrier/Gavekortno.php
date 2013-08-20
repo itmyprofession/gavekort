eff7a9158ab9395e38a2b9f62df975d576081b90
Gopinath <gopinath.c@ewallsolutions.com>
2013-08-20 11:02:13 +0530

    Fixes #11

--- app/code/local/Ewall/GavekortShip/Model/Carrier/Gavekortno.php
+++ app/code/local/Ewall/GavekortShip/Model/Carrier/Gavekortno.php
@@ -1,36 +1,37 @@
 <?php  
-    class Ewall_GavekortShip_Model_Carrier_Gavekortno     
-		extends Mage_Shipping_Model_Carrier_Abstract
-		implements Mage_Shipping_Model_Carrier_Interface
-	{  
-        protected $_code = 'gavekortno';  
-      
-        /** 
-        * Collect rates for this shipping method based on information in $request 
-        * 
-        * @param Mage_Shipping_Model_Rate_Request $data 
-        * @return Mage_Shipping_Model_Rate_Result 
-        */  
-        public function collectRates(Mage_Shipping_Model_Rate_Request $request){  
-            $result = Mage::getModel('shipping/rate_result');  
-            $method = Mage::getModel('shipping/rate_result_method');  
-            $method->setCarrier($this->_code);  
-            $method->setCarrierTitle($this->getConfigData('title'));
-            $method->setMethod($this->_code);  
-            $method->setMethodTitle($this->getConfigData('name'));
-		    $method->setPrice('0.00');
-			$method->setCost('0.00');
-            $result->append($method);  
-            return $result;  
-        }  
 
-		/**
-		 * Get allowed shipping methods
-		 *
-		 * @return array
-		 */
-		public function getAllowedMethods()
-		{
-			return array($this->_code=>$this->getConfigData('name'));
-		}
-    }  
+class Ewall_GavekortShip_Model_Carrier_Gavekortno extends Mage_Shipping_Model_Carrier_Abstract implements Mage_Shipping_Model_Carrier_Interface
+{
+	protected $_code = 'gavekortno';  
+  
+	/** 
+	* Collect rates for this shipping method based on information in $request 
+	* 
+	* @param Mage_Shipping_Model_Rate_Request $data 
+	* @return Mage_Shipping_Model_Rate_Result 
+	*/  
+	public function collectRates(Mage_Shipping_Model_Rate_Request $request){  
+		$result = Mage::getModel('shipping/rate_result');  
+		$method = Mage::getModel('shipping/rate_result_method');  
+		$method->setCarrier($this->_code);  
+		$method->setCarrierTitle($this->getConfigData('title'));
+		$method->setMethod($this->_code);  
+		$method->setMethodTitle($this->getConfigData('name'));
+		if(!$price = $this->getConfigData('price'))
+			$price = 0;
+		$method->setPrice($price);
+		$method->setCost($price);
+		$result->append($method);  
+		return $result;  
+	}  
+
+	/**
+	 * Get allowed shipping methods
+	 *
+	 * @return array
+	 */
+	public function getAllowedMethods()
+	{
+		return array($this->_code=>$this->getConfigData('name'));
+	}
+}
